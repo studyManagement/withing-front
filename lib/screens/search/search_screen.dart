7a2b4a0224ec7304bad7c98560fd56f42f258b35
j1king <jak3b.kim@gmail.com>
2023-08-26 21:45:04 +0900

    refactor: change folder structure

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:withing/components/search_box.dart';
+import 'package:withing/screens/search/search_box.dart';
 
-import '../components/category_selector.dart';
-import '../components/study_list.dart';
-import '../components/study_list_header.dart';
+import 'category_selector.dart';
+import './study_list.dart';
+import './study_list_header.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
