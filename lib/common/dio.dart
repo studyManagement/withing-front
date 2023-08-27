efdfb8177a73ff4973b70bb18032f60dc0956435
Doo <secure3141@gmail.com>
2023-08-27 18:23:05 +0900

    fix: change api localhost path to aws

--- lib/common/dio.dart
+++ lib/common/dio.dart
@@ -1,12 +1,12 @@
 import 'package:dio/dio.dart';
 
-const localhost = 'http://192.168.45.169:8080';
+const api = 'http://3.34.178.101:8080';
 
 // 내 스터디 api
 Future<List> getMyStudyList() async {
   final dio = Dio();
 
-  final resp = await dio.get('$localhost/study/mystudy/5');
+  final resp = await dio.get('$api/study/mystudy/5');
 
   return resp.data['data'];
 }
