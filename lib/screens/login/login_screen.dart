e9871a19cd4d9eaa31334bf1987cbc0c6258d599
Doo <secure3141@gmail.com>
2023-08-27 20:32:47 +0900

    feat: 로그인 화면 구현

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -5,14 +5,68 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Image.asset(
+    return const Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _Title(),
+              _Body(),
+              _Bottom(),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      '스터디 관리도\n즐겁고 쉽게, 위딩',
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 300,
+      color: Colors.grey[200],
+    );
+  }
+}
+
+class _Bottom extends StatelessWidget {
+  const _Bottom();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text('SNS 간편 로그인'),
+        const SizedBox(height: 8),
+        Image.asset(
           'asset/kakao.png',
           width: 50,
           height: 50,
         ),
-      ),
+      ],
     );
   }
 }
