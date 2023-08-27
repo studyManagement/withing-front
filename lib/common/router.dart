dcbe3402b05858cfe1251948a6ddea2f28a21e0f
Doo <secure3141@gmail.com>
2023-08-27 20:29:12 +0900

    feat: 메인 경로 splash 변경 및 login 경로 추가

--- lib/common/router.dart
+++ lib/common/router.dart
@@ -1,8 +1,18 @@
 import 'package:go_router/go_router.dart';
-import 'package:withing/common/root_tab.dart';
+import 'package:withing/screens/login/login_screen.dart';
+import 'package:withing/screens/splash/splash_screen.dart';
 
 final router = GoRouter(
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const RootTab()),
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const SplashScreen(),
+      routes: [
+        GoRoute(
+          path: 'login',
+          builder: (context, state) => const LoginScreen(),
+        ),
+      ],
+    ),
   ],
 );
