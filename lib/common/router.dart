07d63d4dbaf5a80753a4571b716642ae8a577e7c
j1king <jak3b.kim@gmail.com>
2023-09-30 16:46:17 +0900

    feat: implement basic study screen

--- lib/common/router.dart
+++ lib/common/router.dart
@@ -1,8 +1,15 @@
 import 'package:go_router/go_router.dart';
 import 'package:withing/common/root_tab.dart';
+import 'package:withing/screens/search/search_screen.dart';
+import 'package:withing/screens/study/study_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(path: '/', builder: (context, state) => const RootTab()),
+    GoRoute(
+      path: '/studies/:studyId',
+      builder: (context, state) =>
+          StudyScreen(studyId: int.parse(state.pathParameters['studyId']!)),
+    ),
   ],
 );
