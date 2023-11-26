879fccb18139e244886a3b2a5447de3c076457ff
JaewonKim <jak3b.kim@gmail.com>
2023-11-26 16:04:36 +0900

    스터디 상세, 새 스터디 생성 페이지 추가 (#9)
    * feat: implement basic study screen
    
    * feat: implement notice
    
    * feat: implement basic new study screen and badge multi selector

--- lib/common/router.dart
+++ lib/common/router.dart
@@ -1,8 +1,20 @@
 import 'package:go_router/go_router.dart';
 import 'package:withing/common/root_tab.dart';
+import 'package:withing/screens/new-study/new_study_screen.dart';
+import 'package:withing/screens/search/search_screen.dart';
+import 'package:withing/screens/study/study_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(path: '/', builder: (context, state) => const RootTab()),
+    GoRoute(
+      path: '/studies/new',
+      builder: (context, state) => const NewStudyScreen(),
+    ),
+    GoRoute(
+      path: '/studies/:studyId',
+      builder: (context, state) =>
+          StudyScreen(studyId: int.parse(state.pathParameters['studyId']!)),
+    ),
   ],
 );
