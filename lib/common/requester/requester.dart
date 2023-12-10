6da0f3de98616636cc83d649bd02a5279d6a02bd
stias <msn2048@naver.com>
2023-12-10 23:44:55 +0900

    [#4] api requester 구현

--- lib/common/requester/requester.dart
+++ lib/common/requester/requester.dart
@@ -6,9 +6,16 @@ import '../environment/environment.dart';
 
 class Requester {
   static get(String url, Map<String, dynamic> queryParameter) async {
-    final uri = Uri.http(
-        Environment.getEnv(API_SERVER), '/users/check', queryParameter);
+    final uri = Uri.http(Environment.getEnv(API_SERVER), url, queryParameter);
     final response = await http.get(uri);
+
+    return ApiResponse.fromJson(response.body);
+  }
+
+  static post(String url, String bodyParameter) async {
+    final uri = Uri.http(Environment.getEnv(API_SERVER), url);
+    final response = await http.post(uri,
+        headers: {"Content-Type": "application/json"}, body: bodyParameter);
 
     return ApiResponse.fromJson(response.body);
   }
