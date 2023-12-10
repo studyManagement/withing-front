afde0a2df140bd4fa9995ba4770f631ae3ccafa6
stias <msn2048@naver.com>
2023-12-10 23:44:55 +0900

    [#4] api requester 구현

--- lib/models/signup/signup_service.dart
+++ lib/models/signup/signup_service.dart
@@ -1,9 +1,4 @@
-import 'dart:developer';
-
 import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart' as http;
-import 'package:withing/common/const/environment.dart';
-import 'package:withing/common/environment/environment.dart';
 import 'package:withing/common/requester/api_response.dart';
 import 'package:withing/common/requester/requester.dart';
 import 'package:withing/models/signup/signup_model.dart';
@@ -30,18 +25,12 @@ class SignupService extends ChangeNotifier {
   }
 
   Future<bool> signup() async {
-    final uri = Uri.http(Environment.getEnv(API_SERVER), '/users/signup');
-    final response = await http.post(uri,
-        headers: {"Content-Type": "application/json"},
-        body: _signupModel.toJson());
+    ApiResponse apiResponse =
+        Requester.post('/users/signup', _signupModel.toJson());
 
     _signupModel.accessToken = 'test1234';
 
-    log(_signupModel.toJson());
-    log('response code: ${response.statusCode}');
-    log('response body: ${response.body}');
-
-    if (response.statusCode == 400) {
+    if (apiResponse.code == 400) {
       return false;
     }
 
