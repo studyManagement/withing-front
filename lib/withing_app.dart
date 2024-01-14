f8802a759950248a633a0438f94ef352b9bb1d10
stias <msn2048@naver.com>
2024-01-14 23:00:43 +0900

    fix

--- lib/withing_app.dart
+++ lib/withing_app.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 import 'package:withing/common/root_tab.dart';
 import 'package:withing/common/theme/withing_theme.dart';
 import 'package:withing/screens/new-study/new_study_screen.dart';
 import 'package:withing/screens/study/study_screen.dart';
 import 'package:withing/views/login/login_screen.dart';
 import 'package:withing/views/search/screen/search_result_screen.dart';
-import '../models/signup/signup_service.dart';
+
 import '../views/signup/signup_screen.dart';
 
 class WithingApp extends StatelessWidget {
@@ -23,10 +22,7 @@ class WithingApp extends StatelessWidget {
             final provider = state.pathParameters['provider']!;
             final accessToken = state.pathParameters['accessToken']!;
 
-            return Provider(
-              create: (context) => SignupService(),
-              child: SignupScreen(provider, accessToken),
-            );
+            return SignupScreen(provider, accessToken);
           }),
       GoRoute(
         path: '/search/result',
