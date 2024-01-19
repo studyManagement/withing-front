a013e59ae77cf28937e981db7e6127bc2fdbfdf9
stias <msn2048@naver.com>
2024-01-20 05:23:14 +0900

    [#33] feature/signup - fix

--- lib/view_models/signup/signup_viewmodel.dart
+++ lib/view_models/signup/signup_viewmodel.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:withing/common/modal/withing_modal.dart';
@@ -66,4 +64,3 @@ class SignupViewModel extends ChangeNotifier {
     }
   }
 }
-gco
