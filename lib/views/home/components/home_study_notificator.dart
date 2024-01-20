e7de6d56f24f04a0fd6543e6c9fb739e24c89337
stias <msn2048@naver.com>
2024-01-21 04:43:05 +0900

    [#12] feature/home - pageview 작성

--- lib/views/home/components/home_study_notificator.dart
+++ lib/views/home/components/home_study_notificator.dart
@@ -5,201 +5,92 @@ import 'package:withing/common/theme/app/app_colors.dart';
 import 'package:withing/common/theme/app/app_fonts.dart';
 import 'package:withing/view_models/study/study_viewmodel.dart';
 
-class HomeStudyNotificator extends StatelessWidget {
-  HomeStudyNotificator({super.key});
+class HomeStudyNotificationPageView extends StatefulWidget {
+  const HomeStudyNotificationPageView({super.key});
 
-  final PageController _pageController = PageController();
-  int index = 0;
+  @override
+  State<StatefulWidget> createState() => _HomeStudyNotificationPageViewState();
+}
+
+class _HomeStudyNotificationPageViewState
+    extends State<HomeStudyNotificationPageView> {
+  late int selectedPage;
+  late final PageController _pageController;
+
+  @override
+  void initState() {
+    selectedPage = 0;
+    _pageController = PageController(initialPage: selectedPage);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     List<StudyView> studies = context.select<StudyViewModel, List<StudyView>>(
         (provider) => provider.studyViewsInSelectedDay);
 
-    const pageCount = 3;
-
     return (studies.isNotEmpty)
-        ? Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
+        ? Expanded(
             child: Column(
               children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 80,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: AppColors.gray50,
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text('11:00'),
-                        SizedBox(width: 8),
-                        VerticalDivider(
-                            thickness: 1,
-                            width: 10,
-                            indent: 20,
-                            endIndent: 20,
-                            color: Colors.grey),
-                        SizedBox(width: 8),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '최강 마지막 네이버 면접 스터디 파이팅 테스트 테스트',
-                                style: TextStyle(color: Colors.blue),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              SizedBox(height: 2),
-                              Text('실무 면접 및 대면 피드백'),
-                            ],
-                          ),
+                Expanded(
+                  child: PageView.builder(
+                    itemCount: (studies.length / 2).ceil(),
+                    controller: _pageController,
+                    onPageChanged: (page) {
+                      setState(() {
+                        selectedPage = page;
+                      });
+                    },
+                    itemBuilder: (context, index) {
+                      final int minIndex = index * 2;
+                      final List<Widget> widgets = [];
+
+                      widgets.add(HomeStudyNotificationPageViewItem(
+                          studies[minIndex].studyName,
+                          studies[minIndex].explanation,
+                          studies[minIndex].regularMeetings.first.startTime));
+
+                      if (studies.length > minIndex + 1) {
+                        widgets.add(const SizedBox(height: 8));
+                        widgets.add(HomeStudyNotificationPageViewItem(
+                            studies[minIndex + 1].studyName,
+                            studies[minIndex + 1].explanation,
+                            studies[minIndex + 1]
+                                .regularMeetings
+                                .first
+                                .startTime));
+                      }
+
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Column(
+                          children: widgets,
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 80,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: AppColors.gray50,
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text('15:00'),
-                        SizedBox(width: 8),
-                        VerticalDivider(
-                            thickness: 1,
-                            width: 10,
-                            indent: 20,
-                            endIndent: 20,
-                            color: Colors.grey),
-                        SizedBox(width: 8),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '스터디 관리 사이드 프로젝트',
-                              style: TextStyle(color: Colors.blue),
-                            ),
-                            SizedBox(height: 2),
-                            Text('5차 정기 회의(07:30'),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                // PageView(
-                //   controller: _pageController,
-                //   onPageChanged: (newIndex) {
-                //     setState(() {
-                //       index = newIndex;
-                //     });
-                //   },
-                //   children: [
-                //     Container(
-                //       width: MediaQuery.of(context).size.width,
-                //       height: 80,
-                //       decoration: BoxDecoration(
-                //         borderRadius: BorderRadius.circular(12),
-                //         color: Colors.blueGrey[100],
-                //       ),
-                //       child: const Padding(
-                //         padding: EdgeInsets.symmetric(horizontal: 8),
-                //         child: Row(
-                //           mainAxisAlignment: MainAxisAlignment.start,
-                //           children: [
-                //             Text('11:00'),
-                //             SizedBox(width: 8),
-                //             VerticalDivider(thickness: 1, width: 10, indent: 20, endIndent: 20, color: Colors.grey),
-                //             SizedBox(width: 8),
-                //             Expanded(
-                //               child: Column(
-                //                 mainAxisAlignment: MainAxisAlignment.center,
-                //                 crossAxisAlignment: CrossAxisAlignment.start,
-                //                 children: [
-                //                   Text(
-                //                     '최강 마지막 네이버 면접 스터디 파이팅 테스트 테스트',
-                //                     style: TextStyle(color: Colors.blue),
-                //                     overflow: TextOverflow.ellipsis,
-                //                   ),
-                //                   SizedBox(height: 2),
-                //                   Text('실무 면접 및 대면 피드백'),
-                //                 ],
-                //               ),
-                //             )
-                //           ],
-                //         ),
-                //       ),
-                //     ),
-                //     const SizedBox(height: 8),
-                //     Container(
-                //       width: MediaQuery.of(context).size.width,
-                //       height: 80,
-                //       decoration: BoxDecoration(
-                //         borderRadius: BorderRadius.circular(12),
-                //         color: Colors.blueGrey[100],
-                //       ),
-                //       child: const Padding(
-                //         padding: EdgeInsets.symmetric(horizontal: 8),
-                //         child: Row(
-                //           mainAxisAlignment: MainAxisAlignment.start,
-                //           children: [
-                //             Text('15:00'),
-                //             SizedBox(width: 8),
-                //             VerticalDivider(thickness: 1, width: 10, indent: 20, endIndent: 20, color: Colors.grey),
-                //             SizedBox(width: 8),
-                //             Column(
-                //               mainAxisAlignment: MainAxisAlignment.center,
-                //               crossAxisAlignment: CrossAxisAlignment.start,
-                //               children: [
-                //                 Text(
-                //                   '스터디 관리 사이드 프로젝트',
-                //                   style: TextStyle(color: Colors.blue),
-                //                 ),
-                //                 SizedBox(height: 2),
-                //                 Text('5차 정기 회의(07:30'),
-                //               ],
-                //             )
-                //           ],
-                //         ),
-                //       ),
-                //     ),
-                //   ],
-                // ),
-                const SizedBox(height: 8),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: PageViewDotIndicator(
-                    size: const Size(6, 6),
-                    unselectedSize: const Size(6, 6),
-                    currentItem: index,
-                    count: pageCount,
-                    unselectedColor: Colors.black26,
-                    selectedColor: Colors.black,
-                    duration: const Duration(milliseconds: 200),
-                    boxShape: BoxShape.circle,
-                    onItemClicked: (index) {
-                      _pageController.animateToPage(
-                        index,
-                        duration: const Duration(milliseconds: 200),
-                        curve: Curves.easeInOut,
                       );
                     },
                   ),
                 ),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 14),
+                    child: PageViewDotIndicator(
+                      currentItem: selectedPage,
+                      count: (studies.length / 2).ceil(),
+                      unselectedColor: AppColors.gray150,
+                      selectedColor: AppColors.gray700,
+                      boxShape: BoxShape.circle,
+                      size: const Size(5, 5),
+                      unselectedSize: const Size(5, 5),
+                      duration: const Duration(milliseconds: 200),
+                      onItemClicked: (index) {
+                        _pageController.animateToPage(
+                          index,
+                          duration: const Duration(milliseconds: 200),
+                          curve: Curves.easeInOut,
+                        );
+                      },
+                    )),
+                const SizedBox(height: 4),
               ],
             ),
           )
@@ -223,5 +114,72 @@ class HomeStudyNotificator extends StatelessWidget {
               ),
             ),
           );
+  }
+}
+
+class HomeStudyNotificationPageViewItem extends StatelessWidget {
+  final String studyName;
+  final String studyExplain;
+  final String startTime;
+
+  const HomeStudyNotificationPageViewItem(
+      this.studyName, this.studyExplain, this.startTime,
+      {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 66,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: AppColors.gray50,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              startTime.replaceAll('00:00', '00'),
+              style: const TextStyle(
+                color: AppColors.gray400,
+                fontWeight: AppFonts.fontWeight600,
+                fontSize: 14,
+              ),
+            ),
+            const SizedBox(width: 10),
+            const VerticalDivider(
+                thickness: 1.5, width: 10, color: AppColors.gray100),
+            const SizedBox(width: 14),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    studyName,
+                    style: const TextStyle(
+                      color: AppColors.blue500,
+                      fontWeight: AppFonts.fontWeight600,
+                      fontSize: 14,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    studyExplain,
+                    style: const TextStyle(
+                      color: AppColors.gray800,
+                      fontWeight: AppFonts.fontWeight500,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
