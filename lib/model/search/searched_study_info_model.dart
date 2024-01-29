45f496a7324cf6e2b31f00cf876c9527ba04037c
hardy716 <iabc716@icloud.com>
2024-01-29 23:30:31 +0900

    [#59] SearchedStudyInfo 모델 수정

--- lib/model/search/searched_study_info_model.dart
+++ lib/model/search/searched_study_info_model.dart
@@ -6,19 +6,35 @@ part 'searched_study_info_model.g.dart';
 @freezed
 class SearchedStudyInfo with _$SearchedStudyInfo {
   const factory SearchedStudyInfo({
-    required int studyId,
-    required String studyName,
+    required int id,
+    required String? teamName,
     required int max,
-    @JsonKey(name: 'headcount') required int headCount,
+    required int headcount,
+    required String password,
+    required String explanation,
+    required String createdDate,
+    required String deadline,
+    required int leaderId,
     required String? studyImage,
-    required List<int> categories,
-    required int? regularMeetingId,
-    required int gap,
-    required List<int> days,
-    required String startTime, // LocalTime
-    required String endTime, // LocalTime
+    required List<String> categories,
+    required MeetingInfo meetingSchedules,
+    required bool private,
+    required bool finished,
   }) = _SearchedStudyInfo;
 
   factory SearchedStudyInfo.fromJson(Map<String, dynamic> json) =>
       _$SearchedStudyInfoFromJson(json);
+}
+
+@freezed
+class MeetingInfo with _$MeetingInfo {
+  const factory MeetingInfo({
+    required int id,
+    required int day,
+    required String startTime,
+    required String endTime,
+  }) = _MeetingInfo;
+
+  factory MeetingInfo.fromJson(Map<String, dynamic> json) =>
+      _$MeetingInfoFromJson(json);
 }
