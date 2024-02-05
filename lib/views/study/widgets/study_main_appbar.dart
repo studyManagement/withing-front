f7db69ead573f248a871e56289dc9f50d9867cf3
sunjae0902 <sunjaegg0902@naver.com>
2024-02-06 04:31:24 +0900

    StudyMainAppBar 수정

--- lib/views/study/widgets/study_main_appbar.dart
+++ lib/views/study/widgets/study_main_appbar.dart
@@ -2,33 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../../common/theme/app/app_colors.dart';
 
-AppBar studyMainAppBar(BuildContext context, bool isLeader, int studyId) {
-  return AppBar(
-    backgroundColor: AppColors.white,
-    leading: IconButton(
-      icon: const Icon(Icons.arrow_back_ios),
-      onPressed: () => {
-        if (studyId == -1) {context.go('/home')} else {context.pop()}
-      },
-    ),
-    centerTitle: true,
-    foregroundColor: AppColors.black,
-    elevation: 0,
-    actions: <Widget>[
-      Offstage(
-        offstage: (isLeader == true) ? false : true,
-        child: IconButton(
-          icon: Image.asset(
-            'asset/setting.png',
-            width: 32,
-            height: 32,
-          ),
-          onPressed: () => {
-            context.push('/studies/$studyId/manage')
+class StudyMainAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final int studyId;
+  final int leaderId;
 
-          },
-        ),
+  const StudyMainAppBar(
+      {super.key, required this.studyId, required this.leaderId});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: AppColors.white,
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back_ios),
+        onPressed: () => {
+          if (studyId == -1) {context.go('/home')} else {context.pop()}
+        },
       ),
-    ],
-  );
+      centerTitle: true,
+      foregroundColor: AppColors.black,
+      elevation: 0,
+      actions: <Widget>[
+        Offstage(
+          offstage: (leaderId == 24) ? false : true,
+          child: IconButton(
+            icon: Image.asset(
+              'asset/setting.png',
+              width: 32,
+              height: 32,
+            ),
+            onPressed: () => {context.push('/studies/$studyId/manage')},
+          ),
+        ),
+      ],
+    );
+  }
 }
