88e20af284da1fcad74d4b5ce9fc0adb81aa32b6
Yang Seok Yeong <seokyeong.yang@kakao.com>
2024-02-20 03:56:11 +0900

    feature/deeplink - moditeam 으로 딥링크 적용

--- lib/withing_app.dart
+++ lib/withing_app.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:go_router/go_router.dart';
+import 'package:modi/common/logger/logger_service.dart';
 import 'package:modi/common/router/router_service.dart';
 import 'package:modi/common/theme/withing_theme.dart';
+import 'package:uni_links/uni_links.dart';
 
 class WithingApp extends StatelessWidget {
   const WithingApp({super.key});
@@ -9,6 +12,15 @@ class WithingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     FlutterNativeSplash.remove();
+
+    // URI Scheme DeepLink
+    uriLinkStream.listen((Uri? uri) {
+      LoggerService.instance.info("uri: $uri");
+      RouterService.instance.router.go(uri?.path ?? '/');
+    }, onError: (Object err) {
+      LoggerService.instance.error("err: $err");
+    });
+
     return MaterialApp.router(
         theme: WithingTheme.withingThemeData,
         debugShowCheckedModeBanner: false,
