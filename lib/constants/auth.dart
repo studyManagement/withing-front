1033753aae28d14232236401e2b491dc3f24fdf1
sunjae0902 <sunjaegg0902@naver.com>
2024-07-02 20:28:44 +0900

    카카오 인증 키 변경

--- lib/constants/auth.dart
+++ lib/constants/auth.dart
@@ -1,2 +1,2 @@
-const KAKAO_NATIVE_KEY = 'ecb175d3effa9f088359fca3acb0fead';
-const KAKAO_JAVSCRIPT_KEY = '731fb494482653c861c4c07421bdbe9d';
+const KAKAO_NATIVE_KEY = '***REMOVED***';
+const KAKAO_JAVSCRIPT_KEY = '***REMOVED***';
