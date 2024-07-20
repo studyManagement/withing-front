12b1c4b8c7f5397b3d20ceeca4620ee7451c26e4
sunjae0902 <sunjaegg0902@naver.com>
2024-07-20 20:22:55 +0900

    update: spinner

--- lib/common/components/selector/TimeRangeSelector.dart
+++ lib/common/components/selector/TimeRangeSelector.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:modi/common/components/button/confirm_button.dart';
 import 'package:modi/common/components/button/value_button.dart';
@@ -26,177 +27,184 @@ class TimeRangeSelector extends StatefulWidget {
 class _TimeRangeSelectorState extends State<TimeRangeSelector> {
   late DateTime _startAt;
   late DateTime _endAt;
+  late int selectItem;
+  final dateTimeFormatter = DateFormat('a hh:mm', 'ko');
 
   @override
   void initState() {
     super.initState();
-
     _startAt = widget.startAt;
     _endAt = widget.endAt;
+    selectItem = 1;
+  }
+
+  void _updateDateTime(DateTime dateTime) {
+    setState(() {
+      if (selectItem == 1) {
+        _startAt = dateTime;
+      } else {
+        _endAt = dateTime;
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _makeScheduleSelector(
+    widget.onChange(_startAt,_endAt);
+    return InkWell(
+      onTap: () {
+        selectItem = 1; // always start
+        ModiModal.openBottomSheet(
           context,
-          isStartAt: true,
-          startAt: _startAt,
-          endAt: _endAt,
-          onTap: (DateTime startAt, DateTime endAt) {
-            setState(() {
-              _startAt = startAt;
-              _endAt = endAt;
-            });
-            widget.onChange(_startAt, _endAt);
-          },
-        ),
-        const SizedBox(height: 12),
-        _makeScheduleSelector(
-          context,
-          isStartAt: false,
-          startAt: _startAt,
-          endAt: _endAt,
-          onTap: (DateTime startAt, DateTime endAt) {
-            setState(() {
-              _startAt = startAt;
-              _endAt = endAt;
-            });
-            widget.onChange(_startAt, _endAt);
-          },
-        ),
-      ],
-    );
-  }
-
-  GestureDetector _makeScheduleSelector(
-    BuildContext context, {
-    required bool isStartAt,
-    required DateTime startAt,
-    required DateTime endAt,
-    required Function(DateTime, DateTime) onTap,
-  }) {
-    final dateTimeFormatter = DateFormat('a hh:mm', 'ko');
-    int selectItem = isStartAt ? 1 : 2;
-    DateTime currentDateTime = isStartAt ? startAt : endAt;
-
-    return GestureDetector(
-      behavior: HitTestBehavior.translucent,
-      onTap: () => ModiModal.openBottomSheet(
-        context,
-        widget: StatefulBuilder(
-          builder: (BuildContext context, StateSetter setState) {
-            return Column(
-              children: [
-                const Padding(
-                  padding:
-                      EdgeInsets.only(top: 40, left: 16, right: 16, bottom: 20),
-                  child: Column(
-                    children: [
-                      Text(
-                        '시간 선택',
-                        style: TextStyle(
-                          color: AppColors.gray800,
-                          fontWeight: AppFonts.fontWeight600,
-                          fontSize: 16,
+          widget: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(
+                        top: 40, left: 16, right: 16, bottom: 20),
+                    child: Column(
+                      children: [
+                        Text(
+                          '시간 선택',
+                          style: TextStyle(
+                            color: AppColors.gray800,
+                            fontWeight: AppFonts.fontWeight600,
+                            fontSize: 16,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        '일정 투표를 받고 싶은 시간대를 선택해주세요.',
-                        style: TextStyle(
-                          color: AppColors.gray400,
-                          fontSize: 14,
-                          fontWeight: AppFonts.fontWeight500,
+                        SizedBox(height: 8),
+                        Text(
+                          '일정 투표를 받고 싶은 시간대를 선택해주세요.',
+                          style: TextStyle(
+                            color: AppColors.gray400,
+                            fontSize: 14,
+                            fontWeight: AppFonts.fontWeight500,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  height: 70,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: const BoxDecoration(
-                    color: AppColors.gray50,
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    height: 70,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                      color: AppColors.gray50,
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ValueButton(
+                          dateTimeFormatter.format(_startAt),
+                          borderColor: selectItem == 1
+                              ? AppColors.blue400
+                              : AppColors.gray150,
+                          textAlign: TextAlign.center,
+                          onTap: () => {
+                            setState(() {
+                              selectItem = 1;
+                            }),
+                          },
+                        ),
+                        const SizedBox(width: 4),
+                        const Text(
+                          '-',
+                          style: TextStyle(color: AppColors.gray200),
+                        ),
+                        const SizedBox(width: 4),
+                        ValueButton(
+                          dateTimeFormatter.format(_endAt),
+                          borderColor: selectItem == 2
+                              ? AppColors.blue400
+                              : AppColors.gray150,
+                          textAlign: TextAlign.center,
+                          onTap: () => {
+                            setState(() {
+                              selectItem = 2;
+                            }),
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ValueButton(
-                        dateTimeFormatter.format(startAt),
-                        borderColor: selectItem == 1
-                            ? AppColors.blue400
-                            : AppColors.gray150,
-                        textAlign: TextAlign.center,
-                        onTap: () => {
-                          setState(() {
-                            selectItem = 1;
-                          }),
-                        },
-                      ),
-                      const SizedBox(width: 4),
-                      const Text(
-                        '-',
-                        style: TextStyle(color: AppColors.gray200),
-                      ),
-                      const SizedBox(width: 4),
-                      ValueButton(
-                        dateTimeFormatter.format(endAt),
-                        borderColor: selectItem == 2
-                            ? AppColors.blue400
-                            : AppColors.gray150,
-                        textAlign: TextAlign.center,
-                        onTap: () => {
-                          setState(() {
-                            selectItem = 2;
-                          }),
-                        },
-                      ),
-                    ],
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      children: [
+                        TimeSpinner(
+                          focusedBoxWidth: 244,
+                          (selectItem==1)? _startAt:_endAt,
+                              (DateTime dateTime) {
+                            setState(() {
+                              _updateDateTime(dateTime);
+                              widget.onChange(_startAt,_endAt);
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 40),
+                        ConfirmButton(
+                          onTap: () {
+                            widget.onChange(_startAt, _endAt);
+                            Navigator.pop(context);
+                          },
+                          text: '선택 완료',
+                          backgroundColor: AppColors.blue600,
+                          height: 50,
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Column(
-                    children: [
-                      TimeSpinner(currentDateTime, (DateTime dateTime) {
-                        setState(() {
-                          if (selectItem == 1) {
-                            startAt = dateTime;
-                          } else {
-                            endAt = dateTime;
-                          }
-                        });
-                      }),
-                      const SizedBox(height: 40),
-                      ConfirmButton(
-                        onTap: () {
-                          onTap(startAt, endAt);
-                          Navigator.pop(context);
-                        },
-                        text: '선택 완료',
-                        backgroundColor: AppColors.blue600,
-                        height: 50,
-                        width: MediaQuery.of(context).size.width,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
-        height: 496,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
+                ],
+              );
+            },
+          ),
+          height: 496,
+        );
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ValueButton(dateTimeFormatter.format(currentDateTime)),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                  height: 36,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6.0),
+                    border: Border.all(color: AppColors.gray150),
+                  ),
+                  child: Text(dateTimeFormatter.format(_startAt),
+                      style: Theme.of(context).textTheme.bodySmall),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 12),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                  height: 36,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6.0),
+                    border: Border.all(
+                      color: AppColors.gray150,
+                    ),
+                  ),
+                  child: Text(dateTimeFormatter.format(_endAt),
+                      style: Theme.of(context).textTheme.bodySmall),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
